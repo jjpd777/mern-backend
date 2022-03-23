4998113a4cf923b639554cd6862137fbb5cb1e61
juan <jpalacio@axletravel.com>
2022-03-23 03:20:05 -0600

    lessgo

--- routes/record.js
+++ routes/record.js
@@ -15,7 +15,7 @@ const ObjectId = require("mongodb").ObjectId;
 
 
 // This section will help you get a list of all the records.
-recordRoutes.route(CUSTOMERS_TABLE).get(function (req, res) {
+recordRoutes.route("/"+CUSTOMERS_TABLE).get(function (req, res) {
   let db_connect = dbo.getDb(MAIN_TABLE);
   db_connect
     .collection(CUSTOMERS_TABLE)
