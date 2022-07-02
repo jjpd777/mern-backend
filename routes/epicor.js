907e0306dd22e90f4c1470d8d2e5cd6f18be0ac1
juan <jpalacio@axletravel.com>
2022-07-02 12:09:11 -0500

    baby

--- routes/epicor.js
+++ routes/epicor.js
@@ -4,9 +4,14 @@ const CUSTOMERS_TABLE = "terms";
 const recordRoutes = express.Router();
 const dbo = require("../db/conn");
 const ObjectId = require('mongodb').ObjectId;
+const axios = require('axios');
 
 
+const requestHeaders = {
+  Accept: 'application/json',
+};
 
+const options = { headers: requestHeaders };
 
 recordRoutes.route("/insert").post(function (req, response) {
   const path = req.body.path; const data = req.body;
@@ -45,6 +50,47 @@ recordRoutes.route("/read/list/:table").get(function (req, res) {
       if (err) throw err;
       res.json(result);
     });
+});
+
+recordRoutes.route("/insertData").get(function (req, response) {
+  let db_connect = dbo.getDb(MAIN_TABLE);
+  /// read the csv file
+  const dest_exel = "https://firebasestorage.googleapis.com/v0/b/saldada-dev.appspot.com/o/xepelin_sample.xlsx%20-%20Worksheet.tsv?alt=media&token=390ab1a4-2278-40bc-acac-6e9a33d52a46";
+  axios.get(dest_exel, options).then(response => { 
+    const other_data = response.data.split("\r\n");
+    const reference_keys = other_data[0].split("\t");
+    const dbentry = [];
+    other_data.map( (row, ix)=>{
+      if(ix!==0){
+        const d = row.split("\t");
+        const objct = {};
+        d.map( (x, index) =>{
+          objct[reference_keys[index]]= x
+        })
+        dbentry.push(objct);
+      }
+    });
+
+    console.log(dbentry.length)
+
+
+    return response
+  
+  })
+
+  /// write csv file to MongoDB
+  /// potentially, break it down in several steps
+
+  db_connect.collection("sample_xepe").insertOne({"sample":"data"}, function (err, res) {
+    if (err) throw err;
+    response.json(res);
+  });
+});
+
+recordRoutes.route("/receive_information").post(function (req, response) {
+  const path = req.body.path; const data = req.body;
+  let db_connect = dbo.getDb(MAIN_TABLE);
+  console.log(data);
 });
 
 
